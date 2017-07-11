2a33eebecfda95c759eb4519c3cb04ddb55643d7
Daniel Hendricks <mail@danhendricks.com>
2017-07-11 00:02:10 -0500

    I want candy

--- backblaze.php
+++ backblaze.php
@@ -5,7 +5,7 @@
  * Plugin URI:        https://github.com/dmhendricks/backblaze-media-offloader
  * Description:       A simple plugin that allows you to serve your WordPress Media Library files via the Backblaze B2 cloud storage service.
  * Version:           0.2.0
- * Author:            2Lab Internet Services
+ * Author:            Daniel M. Hendricks
  * Author URI:        https://www.danhendricks.com
  * Text Domain:       b2mo
  * Domain Path:       /languages
